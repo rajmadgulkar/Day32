f93ff792d5a56cfb76f79c15a69b2ff769d74d3b
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-04 22:40:16 +0530

    Initial Commit

---
+++ Day30/d.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void printArr(int arr[][3], int arrSize){
+    for(int i=0;i<arrSize;i++){
+    printf("%d\n",*(*arr+i));
+    }
+}
+void main(){
+    int arr[][3]={10,20,30,40,50,60,70,80,90};
+    
+    int arrSize=sizeof(arr)/sizeof(int);
+    printArr(&arr,arrSize);
+
+}
+/*
+10
+20
+30
+40
+50
+60
+70
+80
+90
+*/
