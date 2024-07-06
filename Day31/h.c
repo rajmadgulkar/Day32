3281ba3dae285a9708207db08f7c60ff3fe30d3f
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-06 17:19:10 +0530

    Initial Commit

---
+++ Day31/h.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+    char carr[]={'C','O','R','E','2','W','E','B','\0'};
+    char *str="CORE2WEB";
+    printf("%s\n",carr);
+    printf("%s\n",str);
+    
+     carr[4]='3';
+    printf("%s\n",carr);
+    printf("%s\n",str);
+
+
+ /* 
+    Core Dump
+    *str='B';
+    printf("%s\n",carr);
+    printf("%s\n",str);
+*/
+
+   
+}
+/*
+CORE2WEB
+CORE2WEB
+CORE3WEB
+CORE2WEB
+
+*/
