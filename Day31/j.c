a187cdf46d3475bae368ddec2f529cb12892bd82
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-07 20:13:21 +0530

    Initial Commit

---
+++ Day31/j.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    char *str;//Already address ahe
+    printf("Enter the string:");
+    scanf("%s",str);
+
+    printf("%s\n",str);
+}
+/*
+Enter the string:raj
+(null)
+*/
