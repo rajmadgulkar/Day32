59365b983684f7999bd736f58bcd30c753f9f09d
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-08 18:09:23 +0530

    Initial Commit

---
+++ Day32/a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    char Pname[10];
+    printf("Enter player name:");
+    gets(Pname);
+    puts(Pname);
+
+}
+/*
+Enter player name:Mahendra Singh Dhoni
+Mahendra Singh Dhoni
+*/
