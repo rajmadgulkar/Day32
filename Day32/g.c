1b466c8421516719cec06995688fc821b469f093
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-08 18:54:07 +0530

    Initial Commit

---
+++ Day32/g.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char *str1="Hardik Pandya";
+    char str2[20];
+    puts(str1);
+    puts(str2);
+
+    strcpy(str2,str1);
+
+    puts(str1);
+    puts(str2);
+
+
+}
+/*
+Hardik Pandya
+└┼
+&☻
+Hardik Pandya
+Hardik Pandya
+*/
